505139a8a03e5ec1d20462dec3abbe6b37b47494
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-04-06 00:36:42 +0300

    Added basic functionality of Sack and Gold classes. Also added advanced functionality of interactions between all classes

--- DiggerTask.cs
+++ DiggerTask.cs
@@ -34,25 +34,25 @@ namespace Digger
             switch (Game.KeyPressed)
                 {
                     case Key.Up:
-                        if (y - 1 >= 0)
+                        if (y - 1 >= 0 && Game.Map[x, y - 1] is not Sack)
                         {
                             return new CreatureCommand() { DeltaY = -1};
                         }
                         break;
                     case Key.Down:
-                        if (y + 1 < Game.MapHeight)
+                        if (y + 1 < Game.MapHeight && Game.Map[x, y + 1] is not Sack)
                         {
                             return new CreatureCommand() { DeltaY = 1 };
                         }
                         break;
                     case Key.Left:
-                        if (x - 1 >= 0)
+                        if (x - 1 >= 0 && Game.Map[x -1, y] is not Sack)
                         {
                             return new CreatureCommand() { DeltaX = -1 };
                         }
                         break;
                     case Key.Right:
-                        if (x + 1 < Game.MapWidth)
+                        if (x + 1 < Game.MapWidth && Game.Map[x + 1, y] is not Sack)
                         {
                             return new CreatureCommand() { DeltaX = 1 };
                         }
@@ -66,6 +66,15 @@ namespace Digger
 
         public bool DeadInConflict(ICreature conflictedObject)
         {
+            if (conflictedObject is Gold)
+            {
+                Game.Scores += 10;
+                return false;
+            }
+            if (conflictedObject is Sack)
+            {
+                return true;
+            }
             return false;
         }
 
@@ -77,6 +86,74 @@ namespace Digger
         public string GetImageFileName()
         {
             return "Digger.png";
+        }
+    }
+
+    public class Sack : ICreature
+    {
+        public int FallingDistance = 0;
+
+        public CreatureCommand Act(int x, int y)
+        {
+            if (y + 1 < Game.MapHeight && Game.Map[x, y+1] == null)
+            {
+                FallingDistance += 1;
+                return new CreatureCommand() { DeltaY = 1 };
+            }
+            else if (y + 1 < Game.MapHeight && Game.Map[x, y + 1] is Player && FallingDistance > 0)
+            {
+                return new CreatureCommand() { DeltaY = 1 };
+            }
+            else if ((y == Game.MapHeight - 1 && FallingDistance > 1) || (y + 1 < Game.MapHeight && (Game.Map[x, y + 1] is not Player && Game.Map[x, y + 1] is not null) && FallingDistance > 1))
+            {
+                return new CreatureCommand() { TransformTo = new Gold() };
+            }
+            else
+            {
+                return new CreatureCommand() { DeltaX = 0, DeltaY = 0 };
+            }
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return false;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 1;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Sack.png";
+        }
+    }
+
+    public class Gold : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            return new CreatureCommand() { DeltaX = 0, DeltaY = 0 };
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            if (conflictedObject is Player)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetDrawingPriority()
+        {
+            return 5;
+        }
+
+        public string GetImageFileName()
+        {
+            return "Gold.png";
         }
     }
 }
--- Game.cs
+++ Game.cs
@@ -43,6 +43,6 @@ S TTST  TG
 
 	public static void CreateMap()
 	{
-		Map = CreatureMapCreator.CreateMap(mapWithPlayerTerrain);
+		Map = CreatureMapCreator.CreateMap(mapWithPlayerTerrainSackGold);
 	}
 }
